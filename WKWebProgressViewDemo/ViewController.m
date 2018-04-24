6461508c2361477887f1faaee3e841d83101667d
zry625 <389779553@qq.com>
2018-04-24 18:14:50 +0800

    0424

---
+++ WKWebProgressViewDemo/ViewController.m
@@ -0,0 +1,225 @@
+
+
+#import "ViewController.h"
+#import <WebKit/WebKit.h>
+
+#define kScreenWidth [UIScreen mainScreen].bounds.size.width
+#define kScreenHeight [UIScreen mainScreen].bounds.size.height
+
+@interface ViewController ()<WKUIDelegate,WKNavigationDelegate,UITextFieldDelegate>
+
+@property (nonatomic, strong) WKWebView *wkWebView;
+@property (nonatomic, strong) WKWebViewConfiguration *wkConfig;
+@property (nonatomic, strong) UITextField *textField;
+/*
+ *1.添加UIProgressView属性
+ */
+@property (nonatomic, strong) UIProgressView *progressView;
+
+@end
+
+@implementation ViewController
+
+#pragma mark - 初始化wkWebView
+
+- (WKWebViewConfiguration *)wkConfig {
+    if (!_wkConfig) {
+        _wkConfig = [[WKWebViewConfiguration alloc] init];
+        _wkConfig.allowsInlineMediaPlayback = YES;
+        _wkConfig.allowsPictureInPictureMediaPlayback = YES;
+    }
+    return _wkConfig;
+}
+
+- (WKWebView *)wkWebView {
+    if (!_wkWebView) {
+        _wkWebView = [[WKWebView alloc] initWithFrame:CGRectMake(0, 64, kScreenWidth, kScreenHeight - 64 - 40) configuration:self.wkConfig];
+        _wkWebView.navigationDelegate = self;
+        _wkWebView.UIDelegate = self;
+        [self.view addSubview:_wkWebView];
+    }
+    return _wkWebView;
+}
+
+- (UITextField *)textField {
+    if (!_textField) {
+        //添加地址栏
+        _textField = [[UITextField alloc] initWithFrame:CGRectMake(0, 20, kScreenWidth, 44)];
+        _textField.keyboardType = UIKeyboardTypeURL;
+        _textField.borderStyle = UITextBorderStyleRoundedRect;
+        _textField.returnKeyType = UIReturnKeyDone;
+        _textField.adjustsFontSizeToFitWidth = YES;
+        _textField.delegate = self;
+        [self.view addSubview:_textField];
+    }
+    return _textField;
+}
+
+/*
+ *6.在dealloc中取消监听
+ */
+
+- (void)dealloc {
+    [self.wkWebView removeObserver:self forKeyPath:@"estimatedProgress"];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self setupToolView];
+    
+    /*
+     *2.初始化progressView
+     */
+    self.progressView = [[UIProgressView alloc] initWithFrame:CGRectMake(0, 64, [[UIScreen mainScreen] bounds].size.width, 2)];
+    self.progressView.backgroundColor = [UIColor blueColor];
+    //设置进度条的高度，下面这句代码表示进度条的宽度变为原来的1倍，高度变为原来的1.5倍.
+    self.progressView.transform = CGAffineTransformMakeScale(1.0f, 1.5f);
+    [self.view addSubview:self.progressView];
+    
+    /*
+     *3.添加KVO，WKWebView有一个属性estimatedProgress，就是当前网页加载的进度，所以监听这个属性。
+     */
+    [self.wkWebView addObserver:self forKeyPath:@"estimatedProgress" options:NSKeyValueObservingOptionNew context:nil];
+    
+    [self startLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)setupToolView {
+    UIToolbar *toolBar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, kScreenHeight - 40, kScreenWidth, 40)];
+    [self.view addSubview:toolBar];
+    
+    UIBarButtonItem *fixedSpace = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:self action:nil];
+    UIBarButtonItem *backButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemRewind target:self action:@selector(goBackAction)];
+    UIBarButtonItem *forwardButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFastForward target:self action:@selector(goForwardAction)];
+    UIBarButtonItem *refreshButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemRefresh target:self action:@selector(refreshAction)];
+    
+    [toolBar setItems:@[backButton,fixedSpace,forwardButton,fixedSpace,refreshButton] animated:YES];
+}
+
+#pragma mark - start load web
+
+- (void)startLoad {
+    NSString *urlString = @"http://www.baidu.com";
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:urlString]];
+    request.timeoutInterval = 15.0f;
+    [self.wkWebView loadRequest:request];
+}
+
+#pragma mark - 监听
+
+/*
+ *4.在监听方法中获取网页加载的进度，并将进度赋给progressView.progress
+ */
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSString *,id> *)change context:(void *)context {
+    if ([keyPath isEqualToString:@"estimatedProgress"]) {
+        self.progressView.progress = self.wkWebView.estimatedProgress;
+        if (self.progressView.progress == 1) {
+            /*
+             *添加一个简单的动画，将progressView的Height变为1.4倍
+             *动画时长0.25s，延时0.3s后开始动画
+             *动画结束后将progressView隐藏
+             */
+            __weak typeof (self)weakSelf = self;
+            [UIView animateWithDuration:0.25f delay:0.3f options:UIViewAnimationOptionCurveEaseOut animations:^{
+                weakSelf.progressView.transform = CGAffineTransformMakeScale(1.0f, 1.4f);
+            } completion:^(BOOL finished) {
+                weakSelf.progressView.hidden = YES;
+
+            }];
+        }
+    }else{
+        [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+    }
+}
+
+#pragma mark - WKWKNavigationDelegate Methods
+
+/*
+ *5.在WKWebViewd的代理中展示进度条，加载完成后隐藏进度条
+ */
+
+//开始加载
+- (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation {
+    NSLog(@"开始加载网页");
+    //开始加载网页时展示出progressView
+    self.progressView.hidden = NO;
+    //开始加载网页的时候将progressView的Height恢复为1.5倍
+    self.progressView.transform = CGAffineTransformMakeScale(1.0f, 1.5f);
+    //防止progressView被网页挡住
+    [self.view bringSubviewToFront:self.progressView];
+    self.textField.text = [NSString stringWithFormat:@"%@",webView.URL];
+}
+
+//加载完成
+- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation {
+    NSLog(@"加载完成");
+    //加载完成后隐藏progressView
+//    self.progressView.hidden = YES;
+}
+
+//加载失败
+- (void)webView:(WKWebView *)webView didFailProvisionalNavigation:(WKNavigation *)navigation withError:(NSError *)error {
+    NSLog(@"加载失败");
+    //加载失败同样需要隐藏progressView
+    self.progressView.hidden = YES;
+}
+
+//页面跳转
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler {
+    //允许页面跳转
+    NSLog(@"%@",navigationAction.request.URL);
+    decisionHandler(WKNavigationActionPolicyAllow);
+}
+
+
+#pragma mark - Tool bar item action
+
+- (void)goBackAction {
+    if ([self.wkWebView canGoBack]) {
+        [self.wkWebView goBack];
+    }
+}
+
+- (void)goForwardAction {
+    if ([self.wkWebView canGoForward]) {
+        [self.wkWebView goForward];
+    }
+}
+
+- (void)refreshAction {
+    [self.wkWebView reload];
+}
+
+#pragma mark - UITextField Delegate methods
+
+//开始编辑是时将所有文字选中
+- (void)textFieldDidBeginEditing:(UITextField *)textField {
+    [self.textField performSelector:@selector(selectAll:) withObject:nil];
+}
+
+//点击完成时加载网页
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    if (textField.text.length) {
+        NSString *urlString;
+        if ([textField.text containsString:@"http://"] ||
+            [textField.text containsString:@"https://"]) {
+            urlString = [NSString stringWithFormat:@"%@",textField.text];
+        }else {
+            urlString = [NSString stringWithFormat:@"http://%@",textField.text];
+        }
+        NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:urlString]];
+        request.timeoutInterval = 15.0f;
+        [self.wkWebView loadRequest:request];
+    }
+    [self.textField resignFirstResponder];
+    return YES;
+}
+
+@end
